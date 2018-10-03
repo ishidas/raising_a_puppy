6e89ee4de9af8d85eb63113199d25d2e06b8e409
ishidas <sawako.yoriko@gmail.com>
2018-10-03 09:38:02 -0700

    added server.js

--- src/components/FunArticles/FunArticles.js
+++ src/components/FunArticles/FunArticles.js
@@ -16,8 +16,8 @@ export default class FunArticles extends Component {
 
   Tellus in hac habitasse platea. Urna condimentum mattis pellentesque id nibh tortor. Ornare massa eget egestas purus viverra accumsan in. Dolor sit amet consectetur adipiscing elit. Interdum velit laoreet id donec ultrices tincidunt arcu. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Nunc sed id semper risus in hendrerit gravida rutrum. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Sed euismod nisi porta lorem mollis aliquam ut porttitor leo. Amet risus nullam eget felis eget nunc. Integer enim neque volutpat ac. Nunc sed augue lacus viverra vitae congue eu consequat. Lorem dolor sed viverra ipsum. Blandit massa enim nec dui nunc mattis enim ut tellus. Tincidunt eget nullam non nisi est sit amet. Cursus in hac habitasse platea dictumst quisque sagittis purus sit
         </article><div className={funArticlesStyle.dateobject}>{new Date().toString()}</div></li>
-        <li><article>here2</article></li>
-        <li><article>here3</article></li>
+        <li><article>article 2</article></li>
+        <li><article>article 3</article></li>
       </ul>
     )
   }
--- src/components/NotSoFunList/NotSoFunList.js
+++ src/components/NotSoFunList/NotSoFunList.js
@@ -20,8 +20,8 @@ class NotSoFunList extends Component {
           <li><h2>The Horror of bringing a puppy home</h2>
           <div className={notsofunlistStyle.imageframeright}><img src={babyOlive} /></div><article>Tellus orci ac auctor augue mauris augue neque gravida in. Pulvinar neque laoreet suspendisse interdum consectetur libero. Sagittis id consectetur purus ut faucibus pulvinar elementum. Ullamcorper eget nulla facilisi etiam dignissim diam quis. Sed elementum tempus egestas sed sed risus pretium quam. Viverra nibh cras pulvinar mattis nunc sed. Elit sed vulputate mi sit amet mauris commodo quis. Ac felis donec et odio. Facilisis mauris sit amet massa vitae tortor condimentum. Quisque id diam vel quam elementum pulvinar etiam non quam. In hac habitasse platea dictumst quisque sagittis. Morbi blandit cursus risus at ultrices mi tempus.
           </article><div className={notsofunlistStyle.dateobject}>{new Date().toString()}</div></li>
-          <li><article>here5</article></li>
-          <li><article>here6</article></li>
+          <li><article>article 2</article></li>
+          <li><article>article 3</article></li>
         </ul>
       </div>
     )
